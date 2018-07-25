6b5d906cba0d2c2b6c471571f9fe5761dd6a006e
VinniHashirama <vinicius_freires@hotmail.com>
2018-07-25 14:30:19 -0300

    add and modify scene

--- Marcelinho/Assets/NewScript.cs
+++ Marcelinho/Assets/NewScript.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class NewScript : MonoBehaviour {
 
+   
+
 	// Use this for initialization
 	void Start () {
 		
@@ -14,20 +16,24 @@ public class NewScript : MonoBehaviour {
 		
 	}
 
-<<<<<<< HEAD
-<<<<<<< HEAD
+
+
+
     void EnableComponents()
     {
 
-=======
-    private void Awake()
-    {
-        
->>>>>>> 08d4c896d6274a7f9574895781e6f3ceb6fc7162
-=======
-    private void Awake()
-    {
-        
->>>>>>> c869d5cc34dff9b0bcf971dd565e7f7a5ad4a5ab
     }
+
+
+
+
+    private void Awake()
+    {
+        
+    }
+
+
+
+
+
 }
